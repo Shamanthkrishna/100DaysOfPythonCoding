0c7a6d10abe634a3408c56d405c33578d1250750
Shamanth Krishna <98342024+Shamanthkrishna@users.noreply.github.com>
2023-08-31 22:20:17 +0530

    Add files via upload

---
+++ Day 8 Codes/Function Practice.py
@@ -0,0 +1,14 @@
+#Simple Function
+def greet():
+    print("Hello")
+    print("How are you?")
+    print("How can I help you?")
+
+greet()
+
+#Function taking input
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How are you {name}?")
+
+greet_with_name("Shamanth")
